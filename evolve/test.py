312cd75cae4e0f770f1270934c30faf98b7e593b
sydleither <sydleither@protonmail.com>
2022-10-26 16:53:18 -0400

    add experiment graphing pipeline and start GA outline

---
+++ evolve/test.py
@@ -0,0 +1,22 @@
+import subprocess
+import pandas as pd
+
+N_TYPES = 10
+WORLD_X = 10
+WORLD_Y = 10
+UPDATES = 1000
+MAX_POP = 10000
+SEEDING_PROB = 0.01
+REPRO_THRESHOLD = 5
+REPRO_DILUTION = 0.1
+PROB_CLEAR = 0.1
+
+
+def fitness(diffusion, interaction_matrix):
+    subprocess.run(['../chemical-ecology', f'-DIFFUSION {diffusion} -REPRO_THRESHOLD {REPRO_THRESHOLD} -MAX_POP {MAX_POP} -SEEDING_PROB {SEEDING_PROB} -PROB_CLEAR {PROB_CLEAR} -INTERACTION_SOURCE {interaction_matrix} -WORLD_X {WORLD_X} -WORLD_Y {WORLD_Y} -N_TYPES {N_TYPES}'])
+    df = pd.read_csv('a-eco_data.csv')
+    #calculate fitness
+
+
+if __name__ == '__main__':
+    fitness(0.05, 'evolve/networks/proof_of_concept_interaction_matrix.dat', False)
---
+++ scripts/plot_experiment.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+
+def main():
+    file_location = sys.argv[1]
+
+    df = pd.read_csv(f'{file_location}/all_data.csv')
+    df = df.groupby('Time').mean()
+    plt.plot(df.index, df['mean_Fitness'])
+    plt.xlabel('Time')
+    plt.ylabel('Mean Growth Rate')
+    plt.savefig(f'{file_location}/graph.png')
+
+
+if __name__ == '__main__':
+    main()
