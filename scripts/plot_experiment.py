cb4546167794ff696dc32e0d0299ad4260770ff8
sydleither <sydleither@protonmail.com>
2022-11-14 14:04:07 -0500

    add (rough) error bars to plot and some more mangal data exploration R scripts

--- scripts/plot_experiment.py
+++ scripts/plot_experiment.py
@@ -7,8 +7,10 @@ def main():
     file_location = sys.argv[1]
 
     df = pd.read_csv(f'{file_location}/all_data.csv')
+    std_err = df.groupby('Time')['mean_Fitness'].sem()
     df = df.groupby('Time').mean()
-    plt.plot(df.index, df['mean_Fitness']/max(df['mean_Fitness']))
+
+    plt.errorbar(df.index, df['mean_Fitness']/max(df['mean_Fitness']), yerr=std_err/max(df['mean_Fitness']), color='blue', ecolor='lightblue')
     plt.xlabel('Time')
     plt.ylabel('Mean Growth Rate (normalized)')
     plt.savefig(f'{file_location}/graph.png')
